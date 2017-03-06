9a1a2a6600e88de72c9ff246092598e0e8f88a86
wxcican <wxcican@qq.com>
2017-03-06 10:00:27 +0800

    自定义通知栏样式1

--- app/src/main/java/com/fuicuiedu/xc/my_jpush_20170303/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/my_jpush_20170303/MainActivity.java
@@ -1,7 +1,11 @@
 package com.fuicuiedu.xc.my_jpush_20170303;
 
+import android.app.Notification;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //设置自定义通知栏样式1
+        BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(MainActivity.this);
+        //指定状态栏的小图标
+        builder.statusBarDrawable = R.drawable.aaa;
+        builder.notificationFlags = Notification.FLAG_AUTO_CANCEL
+                | Notification.FLAG_SHOW_LIGHTS;  //设置为自动消失和呼吸灯闪烁
+        builder.notificationDefaults = Notification.DEFAULT_SOUND
+                | Notification.DEFAULT_VIBRATE
+                | Notification.DEFAULT_LIGHTS;  // 设置为铃声、震动、呼吸灯闪烁都要
+        JPushInterface.setPushNotificationBuilder(1, builder);
     }
 }
